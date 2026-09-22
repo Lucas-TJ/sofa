d48f8d12a2446655552165763c926482354d4d3e
Lucas-TJ <lucasbureltojo@gmail.com>
2026-09-22 10:16:43 +0200

    fix TetrahedronCollisionModel and TetrahedronFEMForceField loadInternalStateFrom

--- Sofa/Component/Collision/Geometry/src/sofa/component/collision/geometry/TetrahedronCollisionModel.cpp
+++ Sofa/Component/Collision/Geometry/src/sofa/component/collision/geometry/TetrahedronCollisionModel.cpp
@@ -269,4 +269,11 @@ void TetrahedronCollisionModel::computeBoundingTree(int maxDepth)
     }
 }
 
+void TetrahedronCollisionModel::loadInternalStateFrom(const core::objectmodel::Snapshot::SnapshotObject &snapshot)
+{
+    SOFA_UNUSED(snapshot);
+    tetra = &m_topology->getTetrahedra();
+    resize(tetra->size());
+}
+
 } // namespace sofa::component::collision::geometry
